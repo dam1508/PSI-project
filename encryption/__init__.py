f1e831ea2d7aff7559cba9ef24e11bd10a36a227
kwojewodzki <kwojewodzki99@gmail.com>
2022-06-04 14:02:29 +0200

    Working communication and encryption

---
+++ encryption/__init__.py
@@ -0,0 +1,5 @@
+import sys
+
+if sys.version_info[:2] <= (3, 7):
+    raise ValueError("Python 3.7 or newer is required")
+
---
+++ tcp_communication/tcp_client.py
@@ -0,0 +1,29 @@
+import socket
+from encryption.encrypt import encrypt_data
+
+""" This class defines a C-like struct """
+
+
+def main():
+    server_addr = ('localhost', 2300)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.connect(server_addr)
+        print("Connected to {:s}".format(repr(server_addr)))
+
+        data = encrypt_data("abccabccabccabccabccc", "key")
+
+        s.send(data.encode())
+
+    except AttributeError as ae:
+        print("Error creating the socket: {}".format(ae))
+    except socket.error as se:
+        print("Exception on socket: {}".format(se))
+    finally:
+        print("Closing socket")
+        s.close()
+
+
+if __name__ == "__main__":
+    main()
