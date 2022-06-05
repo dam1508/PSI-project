ccb4d2500ac87c41fddd8d11d0d25d2e1839311f
Fran3x <franex@onet.eu>
2022-06-05 18:47:13 +0200

    config changed

--- tcp_server_dual_stack.py
+++ tcp_server_dual_stack.py
@@ -17,8 +17,8 @@ def readConfigFile(file_path):
   with open(file_path) as f:
     content = json.load(f)
     print(content)
-    HOST = content["HOST"]
-    PORT = content["PORT"]
+    HOST = content["TCP_HOST"]
+    PORT = content["TCP_PORT"]
     HOST_OUT = content["HOST_OUT"]
     PORT_OUT = content["PORT_OUT"]
     XOR_KEY = content["XOR_KEY"]
