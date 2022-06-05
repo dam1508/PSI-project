a2b5d150d1ab6054185d2325f3b1e79f9b7c278a
Damian Piotrowski <damianp.150898@gmail.com>
2022-06-05 16:04:38 +0200

    UDP

--- UDPSender/sender.py
+++ UDPSender/sender.py
@@ -10,30 +10,24 @@ class Payload(Structure):
 
 
 def main():
-    server_addr = ('localhost', 2300)
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+    msgFromClient = 'a'
+    bytesToSend = str.encode(msgFromClient)
+    serverAddressPort = ("127.0.0.1", 20001)
 
-    try:
-        s.connect(server_addr)
-        print("Connected to {:s}".format(repr(server_addr)))
+    bufferSize = 1024
 
-        for i in range(1):
-            print("")
-            payload_out = Payload(1, i, random.uniform(-10, 30))
-            print("Sending id={:d}, counter={:d}, temp={:f}".format(payload_out.id,
-                                                                    payload_out.counter,
-                                                                    payload_out.temp))
-            payload_out = "abcdaaaaaaaaaaaaaaaa".encode()
-            nsent = s.sendall(payload_out)
-    except AttributeError as ae:
-        print("Error creating the socket: {}".format(ae))
-    except socket.error as se:
-        print("Exception on socket: {}".format(se))
-    finally:
-        print("Closing socket")
-        s.close()
+    # Create a UDP socket at client side
 
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+    msg = "Message from Server {}".format(msgFromServer[0])
+
+    print(msg)
 
 if __name__ == "__main__":
     main()
